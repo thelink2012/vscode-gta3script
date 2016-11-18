bf11addf74681d9c6c7099381cecd9b7edb6785d
Denilson M. Amorim <dma_2012@hotmail.com>
2016-11-17 21:01:52 -0300

    Read other XMLs in config/

--- src/controller.ts
+++ src/controller.ts
@@ -26,20 +26,36 @@ export interface CommandsDictionary {
     [command: string]: Command;
 }
 
+export interface AlternatorsDictionary {
+    [alternator: string]: Set<string>;
+}
+
+export interface EnumerationDictionary {
+    [enumName: string]: Array<[string, number]>;
+}
+
+interface ConfigData {
+    commands: CommandsDictionary,
+    alternators: AlternatorsDictionary,
+    enums: EnumerationDictionary,
+}
+
 export class GTA3ScriptController {
 
     private configToken : number;
     private config : string | null;
     private commands: CommandsDictionary | null;
     private commandsById: {};
-    private docs: GTA3DocumentationController;
+    private alternators: {};
+    private enums: {};
 
-    constructor(docs: GTA3DocumentationController) {
+    constructor(private docs: GTA3DocumentationController) {
         this.configToken = 0;
         this.config = null;
         this.commands = {};
         this.commandsById = [];
-        this.docs = docs
+        this.alternators = {};
+        this.enums = {};
     }
 
     /// Whenever the loaded configuration changes, this token changes as well.
@@ -63,6 +79,11 @@ export class GTA3ScriptController {
         return this.commandsById[id];
     }
 
+    /// Gets a specific constant list.
+    public getEnumeration(name: string) : Array<[string, number]> {
+        return this.enums[name];
+    }
+
     /// Gets documentation for the specified command.
     public queryDocumentation(command: Command): Promise<CommandDoc> {
         return this.docs.queryDocumentation(this, command);
@@ -84,22 +105,55 @@ export class GTA3ScriptController {
 
     /// Loads the specified configuration name.
     public loadConfig(configname: string): Thenable<any> {
-        return this.parseConfigPath("C:/Projects/source/gta3script/config/" + configname)
-            .then((value) => {
+        return this.loadConfigPath("C:/Projects/source/gta3script/config/" + configname)
+            .then((config) => {
                 this.config = configname;
-                this.commands = value;
+                this.commands = config.commands;
+                this.alternators = config.alternators;
+                this.enums = config.enums;
                 this.computeByIdTable();
                 ++this.configToken;
             }).catch((err) => {
-                console.log("failed to load gta3sc config", err.message);
+                console.log(`Failed to load config at ${configname}`, err);
             });
     }
 
-    private parseConfigPath(configpath: string): Promise<CommandsDictionary> {
-        return this.parseConfigFile(configpath + "/commands.xml");
+    private loadConfigPath(configpath: string): Promise<ConfigData> {
+        let promiseList = [];
+        
+        let tryParsingConfigFile = (configname): Promise<ConfigData> => {
+            // this promise must always be fulfilled!
+            return new Promise((resolve, reject) => {
+                return this.parseConfigFile(`${configpath}/${configname}`).catch(e => {
+                    console.log(`Failed to load config file ${configname}`, e);
+                    return Promise.resolve({ // ConfigData
+                        commands: {},
+                        alternators: {},
+                        enums: {}
+                    });
+                }).then(cfg => resolve(cfg));
+            });
+        };
+
+        promiseList.push(tryParsingConfigFile("alternators.xml"));
+        promiseList.push(tryParsingConfigFile("commands.xml"));
+        promiseList.push(tryParsingConfigFile("constants.xml"));
+        promiseList.push(tryParsingConfigFile("cleo.xml"));
+
+        return Promise.all(promiseList).then(cfgList => {
+            let allCommands = cfgList.map(c => c.commands);
+            let allAlternators = cfgList.map(c => c.alternators);
+            let allEnums = cfgList.map(c => c.enums);
+
+            return {  // ConfigData
+                commands: Object.assign({}, ...allCommands),
+                alternators: Object.assign({}, ...allAlternators),
+                enums: Object.assign({}, ...allEnums),
+            };
+        });
     }
 
-    private parseConfigFile(configfile: string): Promise<CommandsDictionary> {
+    private parseConfigFile(configfile: string): Promise<ConfigData> {
         return new Promise((resolve, reject) =>{
             fs.readFile(configfile, (err, data) => {
                 if(err) {
@@ -109,38 +163,78 @@ export class GTA3ScriptController {
                     if(err) {
                         return reject(err);
                     }
-                    let map: CommandsDictionary = {}
-                    for(const command of result.GTA3Script.Commands[0].Command) {
-                        let id = null;
-                        let hash = null;
-                        let args = []
 
-                        if(command.$.ID != null)
-                            id = Number(command.$.ID);
-                        if(command.$.Hash != null)
-                            hash = Number(command.$.Hash);
-                        if(command.Args && command.Args[0].Arg && command.Args[0].Arg)
-                            args = command.Args[0].Arg;
+                    if(!result || !result.GTA3Script) {
+                        return reject("Missing GTA3Script node on config XML.")
+                    }
+                    
+                    let commands: CommandsDictionary = {}
+                    let enums: EnumerationDictionary = {};
+                    let alternators: AlternatorsDictionary = {};
 
-                        args = args.map((arg) => { return {
-                            type: arg.$.Type,
-                            enum: arg.$.Enum || null,
-                            entity: arg.$.Entity || null,
-                            description: arg.$.Desc || null,
-                            optional: arg.$.Optional == "true",
-                            out: arg.$.Out == "true",
-                            ref: arg.$.Ref == "true",
-                        }});
+                    let CfgNode = result.GTA3Script;
 
-                        map[command.$.Name] = {
-                            name: command.$.Name,
-                            id: id,
-                            hash: hash,
-                            args: args,
-                        };
+                    if(CfgNode.Commands && CfgNode.Commands.length > 0) {
+                        for(const command of (CfgNode.Commands[0].Command || [])) {
+                            let id = null;
+                            let hash = null;
+                            let args = []
+
+                            if(command.$.ID != null)
+                                id = Number(command.$.ID);
+                            if(command.$.Hash != null)
+                                hash = Number(command.$.Hash);
+                            if(command.Args && command.Args[0].Arg && command.Args[0].Arg)
+                                args = command.Args[0].Arg;
+
+                            args = args.map((arg) => { return {
+                                type: arg.$.Type,
+                                enum: arg.$.Enum || null,
+                                entity: arg.$.Entity || null,
+                                description: arg.$.Desc || null,
+                                optional: arg.$.Optional == "true",
+                                out: arg.$.Out == "true",
+                                ref: arg.$.Ref == "true",
+                            }});
+
+                            commands[command.$.Name] = {
+                                name: command.$.Name,
+                                id: id,
+                                hash: hash,
+                                args: args,
+                            };
+                        }
                     }
 
-                    resolve(map);
+                    if(CfgNode.Alternators && CfgNode.Alternators.length > 0) {
+                        for(const node of CfgNode.Alternators[0].Alternator) {
+                            alternators[node.$.Name] = new Set<string>();
+                            let set = alternators[node.$.Name];
+                            for(const alternative of (node.Alternative || [])) {
+                                set.add(alternative.$.Name);
+                            }
+                        }
+                    }
+
+                    if(CfgNode.Constants && CfgNode.Constants.length > 0) {
+                        for(const node of CfgNode.Constants[0].Enum) {
+                            enums[node.$.Name] = new Array<[string, number]>();
+                            let enumArray = enums[node.$.Name];
+                            let currentValue = 0;
+
+                            for(const constant of (node.Constant || [])) {
+                                if(constant.$.Value != null)
+                                    currentValue = constant.$.Value;
+                                enumArray.push([constant.$.Name, currentValue++])
+                            }
+                        }
+                    }
+
+                    resolve({
+                        commands: commands,
+                        alternators: alternators,
+                        enums: enums,
+                    });
                 });
             })
         });
