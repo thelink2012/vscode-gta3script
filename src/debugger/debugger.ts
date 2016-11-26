55532ec8b862b8d538af999bc297acc4f2db1e80
Denilson M. Amorim <dma_2012@hotmail.com>
2016-11-25 23:30:31 -0300

    Progress

--- src/debugger/debugger.ts
+++ src/debugger/debugger.ts
@@ -26,6 +26,8 @@ interface GTA3DebugClientConnection
 
 class GTA3DebugClient
 {
+	private eventHandlers: {};
+
 	private socket: net.Socket;
 	private handshaked: boolean;
 	private currentSequenceId: number;
@@ -33,6 +35,11 @@ class GTA3DebugClient
 
 	constructor() {
 		this.socket = null;
+		this.eventHandlers = {};
+	}
+
+	public on(event: string, cb: any) {
+		this.eventHandlers[event] = cb;
 	}
 
 	public detach()
@@ -87,7 +94,6 @@ class GTA3DebugClient
 				onTerminate(reason);
 			};
 
-			console.log("UU");
 			socket.connect(port, host, () => {
 				connected = true;
 				this.request('capabilities_get', {}).then((json) => {
@@ -205,7 +211,8 @@ class GTA3DebugClient
 			let handler = this.responseHandlers[json["id"]]
 			if(handler) handler(json);
 		} else if(json["type"] == "event") {
-			// TODO
+			let handler = this.eventHandlers[json["event"]];
+			if(handler) handler(json);
 		}
 	}
 }
@@ -228,37 +235,17 @@ export interface AttachRequestArguments extends DebugProtocol.AttachRequestArgum
 	stopOnEntry?: boolean;
 }
 
-class MockDebugSession extends DebugSession {
+interface VarHandle
+{
+	frame: number,
+	varindex?: number,
+};
+
+class GTA3DebugSession extends DebugSession {
 
 	private debugClient: GTA3DebugClient;
-
-	// we don't support multiple threads, so we can use a hardcoded ID for the default thread
-	private static THREAD_ID = 1;
-
-	// since we want to send breakpoint events, we will assign an id to every event
-	// so that the frontend can match events with breakpoints.
-	private _breakpointId = 1000;
-
-	// This is the next line that will be 'executed'
-	private __currentLine = 0;
-	private get _currentLine() : number {
-		return this.__currentLine;
-    }
-	private set _currentLine(line: number) {
-		this.__currentLine = line;
-		this.sendEvent(new OutputEvent(`line: ${line}\n`));	// print current line on debug console
-	}
-
-	// the initial (and one and only) file we are 'debugging'
-	private _sourceFile: string;
-
-	// the contents (= lines) of the one and only file
-	private _sourceLines = new Array<string>();
-
-	// maps from sourceFile to array of Breakpoints
-	private _breakPoints = new Map<string, DebugProtocol.Breakpoint[]>();
-
-	private _variableHandles = new Handles<string>();
+	private _variableHandles = new Handles<VarHandle>();
+	private breakpointInFile = new Map<string, number[]>();
 
 
 	/**
@@ -268,11 +255,15 @@ class MockDebugSession extends DebugSession {
 	public constructor() {
 		super();
 
-		this.debugClient = new GTA3DebugClient();
-
 		// this debugger uses zero-based lines and columns
 		this.setDebuggerLinesStartAt1(false);
 		this.setDebuggerColumnsStartAt1(false);
+
+		this.debugClient = new GTA3DebugClient();
+		this.debugClient.on('break', (json) => {
+			let thread = Number(json["body"]["script"]["id"]);
+			this.sendEvent(new StoppedEvent("breakpoint", thread));
+		});
 	}
 
 	/**
@@ -290,45 +281,40 @@ class MockDebugSession extends DebugSession {
 		this.sendResponse(response);
 	}
 
+	/**
+	 * Overridden from DebugSession:
+	 * for attach: disconnect from process
+	 * for launch: kill process
+	 */
+	public shutdown(): void {
+		// TODO
+		this.debugClient.detach();
+		super.shutdown();
+	}
+
+	//======================= Launch Request ======================//
+
 	protected launchRequest(response: DebugProtocol.LaunchResponse, args: LaunchRequestArguments): void {
 		// TODO
 	}
 
-	protected attachRequest(response: DebugProtocol.AttachResponse, args: AttachRequestArguments): void
-	{
-		console.log("AA");
+	protected attachRequest(response: DebugProtocol.AttachResponse, args: AttachRequestArguments): void {
 		this.debugClient.attach('localhost', null, ATTACH_TIMEOUT).then((handle) => {
 			handle.onDisconnect = () => {
 				console.log("Disconnecting debugger...");
 				this.sendEvent(new TerminatedEvent());
 			};
 			console.log("Attached!");
-			//this.sendResponse(response);
+			this.breakpointInFile.clear();
+			this.sendResponse(response);
 			this.sendEvent(new InitializedEvent());
-			this.beginDebugMock(response, args);
 		}).catch((e) => {
 			console.log(`Attach failed: ${e}`);
 			this.sendEvent(new TerminatedEvent());
 		});
 	}
 
-	/**
-	 * Overridden from DebugSession:
-	 * attach: disconnect
-	 * launch: kill
-	 */
-	public shutdown(): void {
-		this.debugClient.detach();
-		super.shutdown();
-	}
-
-
-	public request(command: string, args: Object): Thenable<any> {
-		return this.debugClient.request(command, args).catch((e) => {
-			// TODO
-			console.log(`request failed: ${e}`)
-		});
-	}
+	//======================= Basic Requests ======================//
 
 	protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {
 		this.request('script_list', {}).then(json => {
@@ -337,192 +323,230 @@ class MockDebugSession extends DebugSession {
 				response.body.threads.push(new Thread(item.script.id, item.script.name));
 			}
 			this.sendResponse(response);
+		}).catch(e => {
+			this.sendErrorResponse(response, 0, "");
 		});
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	protected beginDebugMock(response: DebugProtocol.AttachResponse, args: AttachRequestArguments) {
-		this._sourceFile = args.program;
-		this._sourceLines = readFileSync(this._sourceFile).toString().split('\n');
-
-		if (args.stopOnEntry) {
-			this._currentLine = 0;
-			this.sendResponse(response);
-
-			// we stop on the first line
-			this.sendEvent(new StoppedEvent("entry", MockDebugSession.THREAD_ID));
-		} else {
-			// we just start to run until we hit a breakpoint or an exception
-			this.continueRequest(<DebugProtocol.ContinueResponse>response, { threadId: MockDebugSession.THREAD_ID });
-		}
 	}
 
 	protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments): void {
 
-		var path = args.source.path;
-		var clientLines = args.lines;
-
-		// read file contents into array for direct access
-		var lines = readFileSync(path).toString().split('\n');
-
+		var path = this.convertClientPathToDebugger(args.source.path);
 		var breakpoints = new Array<Breakpoint>();
+		let addPromises = new Array<Promise<number>>();
+		let remPromises = new Array<Promise<void>>();
 
-		// verify breakpoint locations
-		for (var i = 0; i < clientLines.length; i++) {
-			var l = this.convertClientLineToDebugger(clientLines[i]);
-			var verified = false;
-			if (l < lines.length) {
-				const line = lines[l].trim();
-				// if a line is empty or starts with '+' we don't allow to set a breakpoint but move the breakpoint down
-				if (line.length == 0 || line.indexOf("+") == 0)
-					l++;
-				// if a line starts with '-' we don't allow to set a breakpoint but move the breakpoint up
-				if (line.indexOf("-") == 0)
-					l--;
-				// don't set 'verified' to true if the line contains the word 'lazy'
-				// in this case the breakpoint will be verified 'lazy' after hitting it once.
-				if (line.indexOf("lazy") < 0) {
-					verified = true;    // this breakpoint has been validated
-				}
-			}
-			const bp = <DebugProtocol.Breakpoint> new Breakpoint(verified, this.convertDebuggerLineToClient(l));
-			bp.id = this._breakpointId++;
-			breakpoints.push(bp);
+		if(this.breakpointInFile.has(path)) {
+			this.breakpointInFile.get(path).forEach(id => {
+				remPromises.push(this.request('breakpoint_delete', {"breakpoint": id}));
+			});
+			this.breakpointInFile.set(path, []);
 		}
-		this._breakPoints.set(path, breakpoints);
 
-		// send back the actual breakpoint positions
-		response.body = {
-			breakpoints: breakpoints
-		};
-		this.sendResponse(response);
+		for(let lineno of args.lines)
+		{
+			addPromises.push(new Promise((resolve, reject) => {
+				this.request('breakpoint_set', {
+					"location": {
+						"type": "line",
+						"target": this.convertClientLineToDebugger(lineno),
+						"path": path,
+					},
+					"enabled": true,
+				}).then(json => {
+					return Number(json["body"]["breakpoint"]);
+				});
+			}));
+		}
+
+		Promise.all(remPromises).catch(e => {
+			// ????
+		});
+
+		Promise.all(addPromises).then(ids => {
+			response.body = {
+				breakpoints: ids.map(id => {
+					let breakpoint: DebugProtocol.Breakpoint = new Breakpoint(true);
+					breakpoint.id = Number(id);
+					return breakpoint;
+				})
+			};
+
+			this.breakpointInFile.set(path, ids);
+
+			this.sendResponse(response);
+		}).catch(_ => {
+			this.sendErrorResponse(response, 0, "");
+		});
 	}
 
+	//======================= Step Requests ======================//
 
+	protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {
+		this.request('breakpoint_continue', {}).then(json => {
+			this._variableHandles.reset();
+			this.sendResponse(response);
+		}).catch(e => {
+			this.sendErrorResponse(response, 0, "");
+		})
+	}
 
-	/**
-	 * Returns a fake 'stacktrace' where every 'stackframe' is a word from the current line.
-	 */
+	protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {
+		this.request('breakpoint_continue', {stepaction: "next"}).then(json => {
+			this._variableHandles.reset();
+			this.sendResponse(response);
+		}).catch(e => {
+			this.sendErrorResponse(response, 0, "");
+		})
+	}
+
+	protected stepInRequest(response: DebugProtocol.StepInResponse, args: DebugProtocol.StepInArguments) : void {
+		this.request('breakpoint_continue', {stepaction: "stepin"}).then(json => {
+			this._variableHandles.reset();
+			this.sendResponse(response);
+		}).catch(e => {
+			this.sendErrorResponse(response, 0, "");
+		})
+	}
+
+	protected stepOutRequest(response: DebugProtocol.StepOutResponse, args: DebugProtocol.StepOutArguments) : void {
+		this.request('breakpoint_continue', {stepaction: "stepout"}).then(json => {
+			this._variableHandles.reset();
+			this.sendResponse(response);
+		}).catch(e => {
+			this.sendErrorResponse(response, 0, "");
+		})
+	}
+
+	//======================= Frames Requests ======================//
+
 	protected stackTraceRequest(response: DebugProtocol.StackTraceResponse, args: DebugProtocol.StackTraceArguments): void {
 
-		const words = this._sourceLines[this._currentLine].trim().split(/\s+/);
+		this.request('frame_list', {"script": {"id": args.threadId }}).then(json => {
 
-		const startFrame = typeof args.startFrame === 'number' ? args.startFrame : 0;
-		const maxLevels = typeof args.levels === 'number' ? args.levels : words.length-startFrame;
-		const endFrame = Math.min(startFrame + maxLevels, words.length);
+			let frames = new Array<StackFrame>();
+			let jsonFrames = <any[]>json["body"]["frames"];
 
-		const frames = new Array<StackFrame>();
-		// every word of the current line becomes a stack frame.
-		for (let i= startFrame; i < endFrame; i++) {
-			const name = words[i];	// use a word of the line as the stackframe name
-			frames.push(new StackFrame(i, `${name}(${i})`, new Source(basename(this._sourceFile),
-				this.convertDebuggerPathToClient(this._sourceFile)),
-				this.convertDebuggerLineToClient(this._currentLine), 0));
-		}
-		response.body = {
-			stackFrames: frames,
-			totalFrames: words.length
-		};
-		this.sendResponse(response);
+			const startFrame = typeof args.startFrame === 'number' ? args.startFrame : 0;
+			const maxLevels = typeof args.levels === 'number' ? args.levels : jsonFrames.length-startFrame;
+			const endFrame = Math.min(startFrame + maxLevels, jsonFrames.length);
+			
+			for(let i = startFrame; i < endFrame; ++i)
+			{
+				let jsonFrame = jsonFrames[i];
+				let isLineLocation = jsonFrame.location.type == "line";
+				let sourceFile   = isLineLocation? jsonFrame.location.path : "";
+				let sourceLine   = isLineLocation? jsonFrame.location.target : 0;
+				frames.push(new StackFrame(jsonFrame.frame, "noname", // TODO give a name to this stack frame?
+							   !isLineLocation? undefined : new Source(basename(sourceFile), this.convertDebuggerPathToClient(sourceFile)),
+							   this.convertDebuggerLineToClient(sourceLine), 0));
+			}
+
+			response.body = {
+				stackFrames: frames,
+				totalFrames: jsonFrames.length,
+			};
+			this.sendResponse(response);
+
+		}).catch(e => {
+			this.sendErrorResponse(response, 0, "");
+		})
 	}
 
 	protected scopesRequest(response: DebugProtocol.ScopesResponse, args: DebugProtocol.ScopesArguments): void {
-
-		const frameReference = args.frameId;
-		const scopes = new Array<Scope>();
-		scopes.push(new Scope("Local", this._variableHandles.create("local_" + frameReference), false));
-		scopes.push(new Scope("Closure", this._variableHandles.create("closure_" + frameReference), false));
-		scopes.push(new Scope("Global", this._variableHandles.create("global_" + frameReference), true));
+		let scopes = new Array<Scope>();
+		scopes.push(new Scope("Local", this._variableHandles.create({ frame: args.frameId }), false));
+		scopes.push(new Scope("Global", this._variableHandles.create({ frame: -1 }), true));
 
 		response.body = {
 			scopes: scopes
 		};
+
 		this.sendResponse(response);
 	}
 
 	protected variablesRequest(response: DebugProtocol.VariablesResponse, args: DebugProtocol.VariablesArguments): void {
 
-		const variables = [];
-		const id = this._variableHandles.get(args.variablesReference);
-		if (id != null) {
-			variables.push({
-				name: id + "_i",
-				type: "integer",
-				value: "123",
-				variablesReference: 0
-			});
-			variables.push({
-				name: id + "_f",
-				type: "float",
-				value: "3.14",
-				variablesReference: 0
-			});
-			variables.push({
-				name: id + "_s",
-				type: "string",
-				value: "hello world",
-				variablesReference: 0
-			});
-			variables.push({
-				name: id + "_o",
-				type: "object",
-				value: "Object",
-				variablesReference: this._variableHandles.create("object_")
-			});
+		let variables = new Array<DebugProtocol.Variable>();
+		const varh = this._variableHandles.get(args.variablesReference);
+
+		if(varh.frame == -1)
+		{
+			// TODO
+			this.sendErrorResponse(response, 0, "");
 		}
+		else
+		{
+			if(varh.varindex === undefined)
+			{
+				this.request('frame_varlist', {"frame": varh.frame}).then(json => {
+					for(let vardata of json["body"]["vars"]) {
+						let v = vardata["var"]
 
-		response.body = {
-			variables: variables
-		};
-		this.sendResponse(response);
-	}
+						let respVar = {
+							name: v.name.toLowerCase(),
+							type: v.type,
+							variablesReference: 0,
+							value: "",
+						};
 
-	protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {
+						if(v.count !== undefined) {
+							respVar["indexedVariables"] = v.count;
+							respVar["variablesReference"] = this._variableHandles.create({frame: varh.frame, varindex: v.index });
+							respVar["value"] = `${v.type}[${v.count}]`;
+						} else {
+							respVar["value"] = vardata["value"];
+						}
 
-		for (var ln = this._currentLine+1; ln < this._sourceLines.length; ln++) {
-			if (this.fireEventsForLine(response, ln)) {
-				return;
+						variables.push(respVar)
+					}
+
+					response.body = {
+						variables: variables
+					};
+
+					this.sendResponse(response);
+
+				}).catch(e => {
+					this.sendErrorResponse(response, 0, "");
+				});
+			}
+			else
+			{
+				// TODO improve performance of request (i.e. add a varindex field)
+				this.request('frame_varlist', {"frame": varh.frame}).then(json => {
+					for(let vardata in json["body"]["vars"]) {
+						let v = vardata["var"]
+
+						if(v.index != varh.varindex)
+							continue;
+
+						for(let i = 0; i < v.count; ++i) {
+							variables.push({
+								name: v.name.toLowerCase(),
+								type: v.type,
+								variablesReference: 0,
+								value: vardata["value"][i],
+							});
+						}
+					}
+
+					response.body = {
+						variables: variables
+					};
+					
+					this.sendResponse(response);
+
+				}).catch(e => {
+					this.sendErrorResponse(response, 0, "");
+				});
 			}
 		}
-		this.sendResponse(response);
-		// no more lines: run to end
-		this.sendEvent(new TerminatedEvent());
 	}
 
-	protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {
-
-		for (let ln = this._currentLine+1; ln < this._sourceLines.length; ln++) {
-			if (this.fireStepEvent(response, ln)) {
-				return;
-			}
-		}
-		this.sendResponse(response);
-		// no more lines: run to end
-		this.sendEvent(new TerminatedEvent());
-	}
+	//======================= Evaluate Requests ======================//
 
 	protected evaluateRequest(response: DebugProtocol.EvaluateResponse, args: DebugProtocol.EvaluateArguments): void {
-
+		// TODO
 		response.body = {
 			result: `evaluate(context: '${args.context}', '${args.expression}')`,
 			variablesReference: 0
@@ -530,61 +554,20 @@ class MockDebugSession extends DebugSession {
 		this.sendResponse(response);
 	}
 
-	//---- some helpers
 
-	/**
-	 * Fire StoppedEvent if line is not empty.
-	 */
-	private fireStepEvent(response: DebugProtocol.Response, ln: number): boolean {
+	//======================= Helpers ======================//
 
-		if (this._sourceLines[ln].trim().length > 0) {	// non-empty line
-			this._currentLine = ln;
-			this.sendResponse(response);
-			this.sendEvent(new StoppedEvent("step", MockDebugSession.THREAD_ID));
-			return true;
-		}
-		return false;
+	public request(command: string, args: Object): Promise<any> {
+		return this.debugClient.request(command, args).catch((e) => {
+			// TODO
+			console.log(`request failed: ${e}`)
+			return Promise.reject(e);
+		});
 	}
 
-	/**
-	 * Fire StoppedEvent if line has a breakpoint or the word 'exception' is found.
-	 */
-	private fireEventsForLine(response: DebugProtocol.Response, ln: number): boolean {
-
-		// find the breakpoints for the current source file
-		const breakpoints = this._breakPoints.get(this._sourceFile);
-		if (breakpoints) {
-			const bps = breakpoints.filter(bp => bp.line === this.convertDebuggerLineToClient(ln));
-			if (bps.length > 0) {
-				this._currentLine = ln;
-
-				// 'continue' request finished
-				this.sendResponse(response);
-
-				// send 'stopped' event
-				this.sendEvent(new StoppedEvent("breakpoint", MockDebugSession.THREAD_ID));
-
-				// the following shows the use of 'breakpoint' events to update properties of a breakpoint in the UI
-				// if breakpoint is not yet verified, verify it now and send a 'breakpoint' update event
-				if (!bps[0].verified) {
-					bps[0].verified = true;
-					this.sendEvent(new BreakpointEvent("update", bps[0]));
-				}
-				return true;
-			}
-		}
-
-		// if word 'exception' found in source -> throw exception
-		if (this._sourceLines[ln].indexOf("exception") >= 0) {
-			this._currentLine = ln;
-			this.sendResponse(response);
-			this.sendEvent(new StoppedEvent("exception", MockDebugSession.THREAD_ID));
-			this.sendEvent(new OutputEvent(`exception in line: ${ln}\n`, 'stderr'));
-			return true;
-		}
-
-		return false;
+	protected convertClientPathToDebugger(clientPath: string): string {
+		return super.convertClientPathToDebugger(clientPath).replace(/\\/g, '/');
 	}
 }
 
-DebugSession.run(MockDebugSession);
+DebugSession.run(GTA3DebugSession);
